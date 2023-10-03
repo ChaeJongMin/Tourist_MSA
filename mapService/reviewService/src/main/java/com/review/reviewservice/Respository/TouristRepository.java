dcb350f2f7aae27d689926dd4ad98eeb036cf9ef
채종민(201733336) <90172504+ChaeJongMin@users.noreply.github.com>
2023-10-03 20:44:04 +0900

    Delete mapService/reviewService directory

--- mapService/reviewService/src/main/java/com/review/reviewservice/Respository/TouristRepository.java
+++
@@ -1,8 +0,0 @@
-package com.review.reviewservice.Respository;
-
-import com.review.reviewservice.Domain.TouristEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TouristRepository extends JpaRepository<TouristEntity,Long> {
-
-}
